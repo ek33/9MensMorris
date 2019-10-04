1a1de8e1038d0194c1d5f1b6b54e9a8df0fe2f67
Ekrem <ekremkalabak@gmail.com>
2019-10-04 03:14:33 -0500

    Added basic experimental functionality to try piece placements

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from src.game import Game
 from kivy.app import App
 from kivy.uix.widget import Widget
-
+from kivy.properties import (ObjectProperty, ReferenceListProperty)
 from kivy.core.window import Window
-Window.size = (950, 650)
+Window.size = (970, 650)
 Window.clearcolor = (94/256, 107/256, 98/256, 1)
 
-class NineMenMorrisGame(Widget):
+class Piece(Widget):
     pass
+
+class NineMenMorrisGame(Widget):
+    i=0
+    white1 = ObjectProperty(None)
+    black1 = ObjectProperty(None)
+    white2 = ObjectProperty(None)
+    black2 = ObjectProperty(None)
+    def on_touch_down(self, touch):
+        if self.i == 0:
+            self.white1.pos = touch.x - 25, touch.y - 25
+        elif self.i == 1:
+            self.black1.pos = touch.x - 25, touch.y - 25
+        elif self.i == 2:
+            self.white2.pos = touch.x - 25, touch.y - 25
+        else:
+            self.black2.pos = touch.x - 25, touch.y - 25
+
+        self.i+=1
+
+
+
 
 class NineMenMorrisApp(App):
     def build(self):
