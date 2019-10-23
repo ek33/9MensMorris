b19e3ffc9ef431c7044394f2b29e0486e06440c2
Blake Higgs <blake.higgins36@gmail.com>
2019-10-23 13:02:52 -0500

    Update Cell.py

--- src/game/Cell.py
+++ src/game/Cell.py
@@ -7,12 +7,8 @@ class Cell:
         self.id = id
         self.empty = empty
 
-    def close(self, point):
-        x2 = point.get('x')
-        y2 = point.get('y')
-        #print('{}, {}'.format(x2, y2))
-
-        h_dist = (x2 - self.x) ** 2
-        v_dist = (y2 - self.y) ** 2
+    def close(self, x, y):
+        h_dist = (x - self.x) ** 2
+        v_dist = (y - self.y) ** 2
 
         return math.sqrt(h_dist + v_dist) < self.margin
